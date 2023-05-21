28e63ae9ed00602f59ffeca6be4d35efaf30d522
Rev1le <r.nikiy@yandex.ru>
2023-05-22 00:14:21 +0500

    Added new API command

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,22 @@
 )]
 
 use std::cell::RefCell;
-use std::collections::HashSet;
+use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
 use std::fs::File;
 use std::path::Path;
 use std::sync::Mutex;
 use serde::Serialize;
 use tauri::State;
-use management_core::{SchemaError, CoefficientScheme, Team, Skill, Vacancy};
+use tauri::WindowUrl::App;
+use management_core::{SchemaError, CoefficientScheme, Team, Skill, Vacancy, VacancyCoefficient};
 
 #[derive(Debug)]
 pub enum AppError {
-    SchemaError(SchemaError)
+    SchemaError(SchemaError),
+    Custom {
+        name: String,
+        description: String
+    }
 }
 
 impl From<SchemaError> for AppError {
@@ -41,6 +46,17 @@ impl ManagementApp {
     }
 }
 
+#[derive(Debug, Clone, Serialize)]
+pub struct WorkerRequest {
+    name: String,
+    skills: Vec<String>
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct WorkerResponse {
+    name: String,
+    vacancies: Vec<Vacancy>
+}
 
 #[tauri::command]
 fn get_skills(app: State<'_, Mutex<ManagementApp>>) -> HashSet<Skill> {
@@ -58,6 +74,46 @@ fn get_vacancies(app: State<'_, Mutex<ManagementApp>>) -> HashSet<Vacancy> {
     let skills = schema.schema.get_vacancies().clone();
 
     return skills;
+}
+
+#[tauri::command]
+fn get_vacancies_for_worker(app: State<'_, Mutex<ManagementApp>>, worker: WorkerRequest) -> Result<BTreeMap<String, i64>, AppError> {
+
+    let schema = app.lock().unwrap();
+    let mut vacancies_coefs: BTreeMap<String, i64> = BTreeMap::default();
+
+    for skill in &worker.skills {
+        let skill_info = schema.schema
+            .get_skills()
+            .get(skill)
+            .ok_or(AppError::Custom {
+                name: "Not Found".into(),
+                description: "Skill not found in schema".into()
+            })?;
+
+        let vacancies_coef = skill_info.get_vacancies_coefficient();
+
+        for vac_coef in vacancies_coef {
+
+            let vacancy_name = vac_coef.get_vacancy_name().clone();
+            let coef = vac_coef.get_coefficient();
+
+            vacancies_coefs
+                .entry(vacancy_name)
+                .and_modify(|e| {
+                    println!("{}", e);
+                    *e += coef;
+                })
+                .or_insert(coef);
+        }
+    }
+
+    println!("{:?}", vacancies_coefs);
+
+    //let schema = app.lock().unwrap();
+    //let skills = schema.schema.get_vacancies().clone();
+
+    return Ok(vacancies_coefs);
 }
 
 fn main() {
