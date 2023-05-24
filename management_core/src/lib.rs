70cc5a88e0362872bbd5293eaccd9a52c86ba5f2
Rev1le <r.nikiy@yandex.ru>
2023-05-24 21:49:41 +0500

    Update

--- management_core/src/lib.rs
+++ management_core/src/lib.rs
@@ -77,31 +77,47 @@ impl CoefficientScheme {
         schema_f.read_to_end(&mut schema_bytes)?;
 
         let json: Value = serde_json::from_slice(&schema_bytes).unwrap();
-        //println!("{:?}", json);
+        println!("------------\nJson schema: {:#?}\n------------", json);
 
-        let iter = json["vacancies"]
+        let vacancies =
+            CoefficientScheme::parse_vacancies(&json["vacancies"]);
+        println!("Успешных парсинг вакансий\n------------");
+
+        let skills =
+            CoefficientScheme::parse_skills(&json["skills"], &vacancies);
+        println!("Успешных парсинг навыков\n------------");
+
+        let jobs =
+            CoefficientScheme::parse_jobs(&json["jobs"]);
+        println!("Успешных парсинг работ\n------------");
+
+        return Ok(Self { vacancies, skills, jobs });
+    }
+
+    fn parse_vacancies(value: &Value) -> HashSet<Vacancy> {
+        let iter = value
             .as_array()
             .expect("json конфиг не содержит массива в поле 'vacancies'")
             .iter()
             .map(
                 |vacancy| {
-                    let vacancy_name_str = vacancy.as_str().unwrap().to_owned();
-                    return Vacancy(vacancy_name_str);
+                    let vacancy_name_str = vacancy
+                        .as_str()
+                        .unwrap();
+                    return Vacancy(vacancy_name_str.into());
                 }
             );
 
-        let vacancies: HashSet<Vacancy> = HashSet::from_iter(iter);
+        return HashSet::from_iter(iter);
+    }
 
-        let mut res_skills: HashSet<Skill> = HashSet::default();
-        let skills = json["skills"]
+    fn parse_skills(value: &Value, vacancies: &HashSet<Vacancy>) -> HashSet<Skill> {
+
+        let skills = value
             .as_object()
             .expect("json конфиг не содержит объекта в поле 'skills'");
 
-        let mut schema = Self {
-            vacancies,
-            skills: HashSet::default(),
-            jobs: CoefficientScheme::parse_jobs(&json["jobs"]),
-        };
+        let mut res_skills = HashSet::default();
 
         for (skill_name, vacancies_coef) in skills {
 
@@ -110,45 +126,46 @@ impl CoefficientScheme {
 
             for (vacancy_name, vacancy_coef) in vacancies_coef_map {
 
-                println!("{:?}", vacancy_coef);
                 let vacancy: Vacancy = vacancy_name.clone().into();
-
-                let vacancy_rc = schema.vacancies
+                let vacancy_rc = vacancies
                     .get::<Vacancy>(&vacancy)
                     .ok_or(
                         SchemaError::Custom {
                             name: "Not found vacancy in HasSet".to_owned(),
                             description: format!("В хешсете с вакансиями не найдена вакансия: {:?}", vacancy_name)
                         }
-                    )?;
+                    ).unwrap();
 
-                //println!("{:?}", vacancy_rc);
-                //println!("{:?}", vacancy_coef);
+                let vacancy_coefficient = VacancyCoefficient::new(
+                    &*vacancy_rc,
+                    vacancy_coef.as_i64().unwrap()
+                );
 
-                vacancies_coefficient.push(VacancyCoefficient::new(&*vacancy_rc, vacancy_coef.as_i64().unwrap()));
-                //vacancies_coefficient.push(VacancyCoefficient(Rc::downgrade(vacancy_rc), vacancy_coef.as_i64().unwrap()));
+                vacancies_coefficient.push(vacancy_coefficient);
             }
 
-            schema.skills.insert( Skill {
+            res_skills.insert( Skill {
                 name: skill_name.clone(),
                 vacancies_coefficient
             });
         }
 
-        Ok(schema)
+        return res_skills;
     }
 
     fn parse_jobs(value: &Value) -> HashSet<Job> {
 
-        let mut jobs = HashSet::default();
-        let jobs_json = value.as_object().unwrap()["companies"].as_object().unwrap();
+        let jobs_map = value
+            .as_object()
+            .expect("json конфиг не содержит объекта в поле 'jobs'")
+            ["companies"]
+            .as_object()
+            .expect("json конфиг не содержит объекта в поле 'companies'");
 
-        for job in jobs_json {
-            jobs.insert(Job(job.0.clone()));
-        }
-
-        return jobs
-
+        return jobs_map
+            .into_iter()
+            .map(|job| Job(job.0.into()))
+            .collect::<HashSet<Job>>();
     }
 
     // Not use pls
--- src/main.rs
+++ src/main.rs
@@ -63,6 +63,7 @@ fn get_skills(app: State<'_, Mutex<ManagementApp>>) -> HashSet<Skill> {
 
     let schema = app.lock().unwrap();
     let skills = schema.schema.get_skills().clone();
+    println!("Возвращены навыки\n------------");
 
     return skills;
 }
@@ -71,9 +72,10 @@ fn get_skills(app: State<'_, Mutex<ManagementApp>>) -> HashSet<Skill> {
 fn get_vacancies(app: State<'_, Mutex<ManagementApp>>) -> HashSet<Vacancy> {
 
     let schema = app.lock().unwrap();
-    let skills = schema.schema.get_vacancies().clone();
+    let vacancies = schema.schema.get_vacancies().clone();
+    println!("Возвращены должности\n------------");
 
-    return skills;
+    return vacancies;
 }
 
 #[tauri::command]
@@ -86,17 +88,19 @@ fn get_jobs(app: State<'_, Mutex<ManagementApp>>) -> HashSet<Job> {
 }
 
 #[tauri::command]
-fn get_vacancies_for_worker(app: State<'_, Mutex<ManagementApp>>, worker: WorkerRequest) -> WorkerResponse {
+fn get_vacancies_for_worker(
+    app: State<'_, Mutex<ManagementApp>>,
+    worker: WorkerRequest)
+    -> WorkerResponse {
 
     let schema = app.lock().unwrap();
+    let schema_skills = schema.schema.get_skills();
+
     let mut vacancies_coefs: BTreeMap<String, i64> = BTreeMap::default();
 
-    println!("{:#?}", schema.schema.get_skills());
-
     for skill in &worker.skills {
-        println!("{}", skill);
-        let skill_info = schema.schema
-            .get_skills()
+
+        let skill_info = schema_skills
             .get(skill)
             .ok_or(AppError::Custom {
                 name: "Not Found".into(),
@@ -113,21 +117,23 @@ fn get_vacancies_for_worker(app: State<'_, Mutex<ManagementApp>>, worker: Worker
             vacancies_coefs
                 .entry(vacancy_name)
                 .and_modify(|e| {
-                    println!("{}", e);
                     *e += coef;
                 })
                 .or_insert(coef);
         }
     }
 
-    println!("{:?}", vacancies_coefs);
-
-    let mut vac = vacancies_coefs.clone().into_iter().collect::<Vec<(String, i64)>>();
+    // Сортировка коеффициентов у должностей
+    /*
+    let mut vac = vacancies_coefs
+        .clone()
+        .into_iter()
+        .collect::<Vec<(String, i64)>>();
     vac.sort_by_key(|vac| vac.1);
-    println!("Отсорт массив: {:?}", vac);
 
-    //let schema = app.lock().unwrap();
-    //let skills = schema.schema.get_vacancies().clone();
+     */
+
+    println!("Возвращены должности для работника: {}\n------------", worker.name);
 
     return WorkerResponse {
         name: worker.name,
@@ -136,15 +142,21 @@ fn get_vacancies_for_worker(app: State<'_, Mutex<ManagementApp>>, worker: Worker
 }
 
 fn main() {
-    // tauri::Builder::default()
-    //     .manage(
-    //         Mutex::new(ManagementApp::new(Path::new("./skill_coefficients.json")).unwrap())
-    //     )
-    //     .invoke_handler(tauri::generate_handler![get_skills, get_vacancies, get_vacancies_for_worker, get_jobs])
-    //     .run(tauri::generate_context!())
-    //     .expect("error while running tauri application");
 
-    tests::test();
+    let management_app = ManagementApp::new(Path::new("./skill_coefficients.json")).unwrap();
+
+    tauri::Builder::default()
+        .manage(Mutex::new(management_app))
+        .invoke_handler(tauri::generate_handler![
+            get_skills,
+            get_vacancies,
+            get_vacancies_for_worker,
+            get_jobs
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
+    // tests::test();
 }
 
 mod tests {
